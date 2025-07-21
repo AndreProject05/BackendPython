af263845db6cd2b87f890cb147998798daf9bbc2
Andre <andreuser0705@gmail.com>
2025-07-21 09:58:46 +0200

    aggiunto script python + gitignore

---
+++ backend.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_hostname():
+    hostname = socket.gethostname()
+    return f"Nome host della macchina: {hostname}"
+
+if __name__ == '__main__':
+    # Avvia il server sulla porta 80 su tutte le interfacce
+    app.run(host='0.0.0.0', port=8080)
